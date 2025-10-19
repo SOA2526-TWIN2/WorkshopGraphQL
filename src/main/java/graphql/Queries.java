912a59193a95a81ed4f0907f9bfa2928f7e669b2
Oumeima-IbnElfekih <oumeima.ibnelfekih@esprit.tn>
2025-10-19 22:45:06 +0100

    first commit

---
+++ src/main/java/graphql/Queries.java
@@ -0,0 +1,20 @@
+package graphql;
+
+import com.coxautodev.graphql.tools.GraphQLRootResolver;
+import entities.Module;
+import repository.ModuleBusiness;
+
+import java.util.List;
+
+public class Queries implements GraphQLRootResolver {
+    public static ModuleBusiness mb= new ModuleBusiness();
+
+    public Queries(ModuleBusiness mb) {
+        this.mb=mb;
+    }
+
+    public List<Module> allModules(){
+
+        return mb.getAllModules();
+    }
+}
