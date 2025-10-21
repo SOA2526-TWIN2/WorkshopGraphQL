ddc831bb0940ae4b8a362b7e3cabba836954a722
Oumeima-IbnElfekih <oumeima.ibnelfekih@esprit.tn>
2025-10-21 21:10:02 +0100

    version finale

--- src/main/java/graphql/Queries.java
+++ src/main/java/graphql/Queries.java
@@ -2,19 +2,42 @@ package graphql;
 
 import com.coxautodev.graphql.tools.GraphQLRootResolver;
 import entities.Module;
+import entities.UniteEnseignement;
 import repository.ModuleBusiness;
+import repository.UniteEnseignementBusiness;
 
 import java.util.List;
 
 public class Queries implements GraphQLRootResolver {
     public static ModuleBusiness mb= new ModuleBusiness();
-
-    public Queries(ModuleBusiness mb) {
+    public static UniteEnseignementBusiness ueb =new UniteEnseignementBusiness();
+    public Queries(ModuleBusiness mb,UniteEnseignementBusiness ueb) {
         this.mb=mb;
+        this.ueb= ueb;
     }
 
     public List<Module> allModules(){
-
         return mb.getAllModules();
+    }
+    public List<Module> getModulesByType(Module.TypeModule type) {
+        return  mb.getModulesByType(type);
+    }
+    public List<UniteEnseignement> getAllUE() {
+        return ueb.getListeUE();
+    }
+
+
+    public UniteEnseignement getUEByCode(int code) {
+        return ueb.getUEByCode(code);
+    }
+
+
+    public List<UniteEnseignement> getUEByDomaine(String domaine) {
+        return ueb.getUEByDomaine(domaine);
+    }
+
+
+    public List<UniteEnseignement> getUEBySemestre(int semestre) {
+        return ueb.getUEBySemestre(semestre);
     }
 }
